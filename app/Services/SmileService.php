ac4e364b64ab2c4b0029d39b128a358dcc25ff78
skozekai-bit <skozekai@gmail.com>
2026-02-07 12:17:07 +0530

    Create SmileService.php to handle Smile.one API interactions

---
+++ app/Services/SmileService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+class SmileService {
+    public function verifyMobileLegendPlayer($playerId) {
+        // Implementation goes here
+    }
+
+    public function createTopUpOrder($orderDetails) {
+        // Implementation goes here
+    }
+
+    public function processTopUp($topUpId) {
+        // Implementation goes here
+    }
+
+    public function retryTransaction($transactionId) {
+        // Implementation goes here
+    }
+
+    public function checkTransactionStatus($transactionId) {
+        // Implementation goes here
+    }
+
+    public function validateWebhook($payload) {
+        // Implementation goes here
+    }
+}
